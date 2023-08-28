c2e02579c58dcce82050b17c09f82fea6a52ae8e
Andy Shen <aashen@berkeley.edu>
2023-08-28 14:48:39 -0700

    add argument for number of clusters

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -1,6 +1,7 @@
 # For Jonathan: Use roxygen to document the code
 
-dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2) {
+dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
+                               region = NULL) {
   # insert code here
   # Suggestions:
   #  use the "irlba" R package for PCA/SVD
