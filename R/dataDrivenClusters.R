187dc8714d50bb6cc8f7727c110800269c076f55
Andy Shen <aashen@berkeley.edu>
2023-08-28 14:24:15 -0700

    few commits

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -1,5 +1,9 @@
-# Use roxygen to document the code
+# For Jonathan: Use roxygen to document the code
 
 dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2) {
   # insert code here
+  # Suggestions:
+  #  use the "irlba" R package for PCA/SVD
+  #  use the "umap" package for umap
+  # Ensure the output is in list format. Data frames should be tibbles.
 }
