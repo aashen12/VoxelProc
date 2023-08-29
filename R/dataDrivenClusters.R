bdc3749393520057ee14d33d335496665bd0a974
Andy <aashen@berkeley.edu>
2023-08-28 23:25:03 -0700

    fixed style. see PR for other comments

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -6,7 +6,7 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
                                region = NULL) {
 
   # remove all columns that sum to 0
-  voxel_df <- voxel_df[,colSums(voxel_df) != 0]
+  voxel_df <- voxel_df[, colSums(voxel_df) != 0]
   xyz <- voxel_df[, 1:3]
   voxel_df <- voxel_df[, 4:ncol(voxel_df)]
 
@@ -27,35 +27,28 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
   umap_df <- as.data.frame(umap_coords)
 
   #apply kmeans to umap-reduced data
-
   km <- kmeans(umap_df, centers = n_clust, iter.max = 5000)
 
   #obtain the dataframe with xyz coords, UMAP coords, and cluster values
-
   data_df <- cbind(cbind(xyz, umap_coords), km$cluster)
 
   #obtain a dataframe with umap coords and kmeans clusters to make plotting easier
-
   plot_df <- as.data.frame(cbind(umap_coords, km$cluster))
   colnames(plot_df) <- c("umap_coord_1", "umap_coord_2", "clusters")
 
-  #creating an if statement in case visualization isn't possible because n_umap > 2
-
-  if (n_umap > 2){
-
-  plot <- ggplot(plot_df, aes(x = umap_coord_1, y = umap_coord_2, color = clusters)) + geom_point()
-
-  result <- list(data_df, plot_df, plot)
-
+  if (n_umap == 2) {
+    plot <- plot_df %>%
+      ggplot(aes(x = umap_coord_1, y = umap_coord_2, color = clusters)) +
+      geom_point() +
+      theme_bw() +
+      labs(x = "UMAP 1", y = "UMAP 2", title = region)
+    result <- list(data_df = data_df, plot = plot)
   }
-
   else {
-    print("n_umap > 2, so no visualization is possible")
+    print("n_umap is greater than 2, so no visualization is possible")
+    result <- list(data_df = data_df)
   }
 
   #the return object will consist of two dataframes, data_df and plot_df
-
-  result <- list(data_df, plot_df)
-
   return(result)
 }
