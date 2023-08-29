75a2cee9aaff045f8b7d1cd29aaa196d0ce722cc
Jonathan Lin <jlin900@berkeley.edu>
2023-08-28 19:48:46 -0700

    began rough sketch on function

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -1,10 +1,44 @@
-# For Jonathan: Use roxygen to document the code
+#' @import irlba
+#' @import umap
+
 
 dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
                                region = NULL) {
-  # insert code here
-  # Suggestions:
-  #  use the "irlba" R package for PCA/SVD
-  #  use the "umap" package for umap
-  # Ensure the output is in list format. Data frames should be tibbles.
+
+  #remove all columns that sum to 0
+
+  voxel_df <- voxel_df[,colSums(voxel_df) != 0]
+
+  #scale the data
+
+  voxel_df <- scale(voxel_df, center=TRUE, scale=TRUE)
+
+  #perform PCA on data
+
+  pca <- prcomp_irlba(voxel_df, n=n_pca, retx=TRUE)
+  rot_data <- pca$x
+  pcs <- pca$rotation
+
+  #perform umap
+
+  umap_sim <- umap(pcs, n_components = n_umap, preserve.seed= TRUE)
+  umap_coords <- umap_sim$layout
+  umap_df <- as.data.frame(umap_coords)
+
+  #apply kmeans to umap-reduced data
+
+  km <- kmeans(umap_df, centers = n_clust, iter.max = 5000)
+
+  #obtain the dataframe with xyz coords, UMAP coords, and cluster values
+
+  data_df <- cbind(cbind(voxel_df, umap_coords), km$cluster)
+
+  #obtain a dataframe with umap coords and kmeans clusters to make plotting easier
+
+  plot_df <- cbind(umap_coords, km$cluster)
+  colnames(plot_df) <- c('umap coord 1', 'umap coord 2', 'clusters')
+  plot <- ggplot(plot_df, aes(x = 'umap coord 1', y = 'umap coord 2', color = 'clusters'))
+  + geom_point()
+
+  return(data_df, plot)
 }
