a1151f4525f3980197babc1661cd0ccb1eb136c5
Jonathan Lin <jlin900@berkeley.edu>
2023-08-28 20:31:09 -0700

    fixed some errors

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -8,6 +8,8 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
   #remove all columns that sum to 0
 
   voxel_df <- voxel_df[,colSums(voxel_df) != 0]
+  xyz <- voxel_df[,1:3]
+  voxel_df <- voxel_df[,4:ncol(voxel_df)]
 
   #scale the data
 
@@ -21,7 +23,7 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
 
   #perform umap
 
-  umap_sim <- umap(pcs, n_components = n_umap, preserve.seed= TRUE)
+  umap_sim <- umap(rot_data, n_components = n_umap, preserve.seed= TRUE)
   umap_coords <- umap_sim$layout
   umap_df <- as.data.frame(umap_coords)
 
@@ -31,14 +33,13 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
 
   #obtain the dataframe with xyz coords, UMAP coords, and cluster values
 
-  data_df <- cbind(cbind(voxel_df, umap_coords), km$cluster)
+  data_df <- cbind(cbind(xyz, umap_coords), km$cluster)
 
   #obtain a dataframe with umap coords and kmeans clusters to make plotting easier
 
-  plot_df <- cbind(umap_coords, km$cluster)
+  plot_df <- as.data.frame(cbind(umap_coords, km$cluster))
   colnames(plot_df) <- c('umap coord 1', 'umap coord 2', 'clusters')
-  plot <- ggplot(plot_df, aes(x = 'umap coord 1', y = 'umap coord 2', color = 'clusters'))
-  + geom_point()
+  plot <- ggplot(plot_df, aes(x = 'umap coord 1', y = 'umap coord 2', color = 'clusters')) + geom_point()
 
   return(data_df, plot)
 }
