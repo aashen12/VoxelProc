17afb329e0c9eefc3671a1f38e592b4063e74ddf
Andy Shen <andyshen@Andys-MacBook-Air.local>
2023-08-29 08:44:29 -0700

    deleted extra lines.

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -6,32 +6,25 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
                                region = NULL) {
 
   # failsafe in case n_pca or UMAP < 2
-
   if (n_pca < 2 || n_umap < 2){
-
-    stop("either n_pca < 2 or n_umap < 2, so no visualization is possible")
-
+    stop("Number of principal and UMAP components must be at least 2.")
   }
 
   else {
-
   # remove all columns that sum to 0
   voxel_df <- voxel_df[, colSums(voxel_df) != 0]
   xyz <- voxel_df[, 1:3]
   voxel_df <- voxel_df[, 4:ncol(voxel_df)]
 
   # scale the data
-
   voxel_df <- scale(voxel_df, center = TRUE, scale = TRUE)
 
   # perform PCA on data
-
   pca <- prcomp_irlba(voxel_df, n = n_pca, retx = TRUE)
   rot_data <- pca$x
   pcs <- pca$rotation
 
   # perform umap
-
   umap_sim <- umap(rot_data, n_components = n_umap, preserve.seed = TRUE)
   umap_coords <- umap_sim$layout
   umap_df <- as.data.frame(umap_coords)
