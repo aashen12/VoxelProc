46e68bc618896a9884ef73ea01a46be425ca10c7
Andy <aashen@berkeley.edu>
2023-08-28 23:19:20 -0700

    a few style changes

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -5,25 +5,24 @@
 dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
                                region = NULL) {
 
-  #remove all columns that sum to 0
-
+  # remove all columns that sum to 0
   voxel_df <- voxel_df[,colSums(voxel_df) != 0]
-  xyz <- voxel_df[,1:3]
-  voxel_df <- voxel_df[,4:ncol(voxel_df)]
+  xyz <- voxel_df[, 1:3]
+  voxel_df <- voxel_df[, 4:ncol(voxel_df)]
 
   #scale the data
 
-  voxel_df <- scale(voxel_df, center=TRUE, scale=TRUE)
+  voxel_df <- scale(voxel_df, center = TRUE, scale = TRUE)
 
   #perform PCA on data
 
-  pca <- prcomp_irlba(voxel_df, n=n_pca, retx=TRUE)
+  pca <- prcomp_irlba(voxel_df, n = n_pca, retx = TRUE)
   rot_data <- pca$x
   pcs <- pca$rotation
 
   #perform umap
 
-  umap_sim <- umap(rot_data, n_components = n_umap, preserve.seed= TRUE)
+  umap_sim <- umap(rot_data, n_components = n_umap, preserve.seed = TRUE)
   umap_coords <- umap_sim$layout
   umap_df <- as.data.frame(umap_coords)
 
