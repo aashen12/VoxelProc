f7bdd554fde64e54143a76566f87a2e7ca6dba70
Jonathan Lin <jlin900@berkeley.edu>
2023-08-29 07:44:08 -0700

    added failsafe for n_pca < 2 and n_umap < 2

--- R/dataDrivenClusters.R
+++ R/dataDrivenClusters.R
@@ -5,34 +5,44 @@
 dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
                                region = NULL) {
 
+  # failsafe in case n_pca or UMAP < 2
+
+  if (n_pca < 2 || n_umap < 2){
+
+    stop("either n_pca < 2 or n_umap < 2, so no visualization is possible")
+
+  }
+
+  else {
+
   # remove all columns that sum to 0
   voxel_df <- voxel_df[, colSums(voxel_df) != 0]
   xyz <- voxel_df[, 1:3]
   voxel_df <- voxel_df[, 4:ncol(voxel_df)]
 
-  #scale the data
+  # scale the data
 
   voxel_df <- scale(voxel_df, center = TRUE, scale = TRUE)
 
-  #perform PCA on data
+  # perform PCA on data
 
   pca <- prcomp_irlba(voxel_df, n = n_pca, retx = TRUE)
   rot_data <- pca$x
   pcs <- pca$rotation
 
-  #perform umap
+  # perform umap
 
   umap_sim <- umap(rot_data, n_components = n_umap, preserve.seed = TRUE)
   umap_coords <- umap_sim$layout
   umap_df <- as.data.frame(umap_coords)
 
-  #apply kmeans to umap-reduced data
+  # apply kmeans to umap-reduced data
   km <- kmeans(umap_df, centers = n_clust, iter.max = 5000)
 
-  #obtain the dataframe with xyz coords, UMAP coords, and cluster values
+  # obtain the dataframe with xyz coords, UMAP coords, and cluster values
   data_df <- cbind(cbind(xyz, umap_coords), km$cluster)
 
-  #obtain a dataframe with umap coords and kmeans clusters to make plotting easier
+  # obtain a dataframe with umap coords and kmeans clusters to make plotting easier
   plot_df <- as.data.frame(cbind(umap_coords, km$cluster))
   colnames(plot_df) <- c("umap_coord_1", "umap_coord_2", "clusters")
 
@@ -49,6 +59,7 @@ dataDrivenClusters <- function(voxel_df, n_pca = 20, n_umap = 2, n_clust = 2,
     result <- list(data_df = data_df)
   }
 
-  #the return object will consist of two dataframes, data_df and plot_df
+  # the return object will consist of two dataframes, data_df and plot_df
   return(result)
+  }
 }
