58ccd356386359e34eb7ec200c818a7d03ba0f12
Jonathan Lin <jlin900@berkeley.edu>
2023-09-01 10:41:12 -0700

    updated tests again (but they still dont work)

--- tests/testthat/test-dataDrivenClusters.R
+++ tests/testthat/test-dataDrivenClusters.R
@@ -38,13 +38,15 @@ test_that(paste("dataDrivenClusters returns data_df as a tibble"),
           })
 
 # this last test will be updated more, right now it is quite crude
-test_that(paste("dataDrivenClusters returns the correct result given the
-                specified pipeline"),
+test_that(paste("dataDrivenClusters returns the correct UMAP coordinates"),
           {
+            set.seed(5284)
+            result <- dataDrivenClusters(data)
             xyz <- data[,1:3]
             voxel_df <- data[,4:ncol(data)]
             voxel_df <- scale(voxel_df, center=TRUE, scale=TRUE)
             pca <- prcomp_irlba(voxel_df, n = 20, retx = TRUE)
+
             rot_data <- pca$x
             pcs <- pca$rotation
             umap_sim <- umap(rot_data, n_components = 2, preserve.seed = TRUE)
@@ -55,7 +57,15 @@ test_that(paste("dataDrivenClusters returns the correct result given the
             colnames(sim_data_df)[4:5] <- c("U1", "U2")
             sim_data_df <- as_tibble(sim_data_df)
 
+            plot <- sim_data_df %>%
+              ggplot(aes(x = U1, y = U2, color = factor(cluster))) +
+              geom_point() +
+              theme_bw() +
+              labs(x = "UMAP 1", y = "UMAP 2", title = NULL, color = "Clusters")
 
-            expect_equal(sim_data_df[,4], result$data_df[,4])
-            expect_equal(sim_data_df[,5], result$data_df[,5])
+            sim_result <- list(data_df = sim_data_df, plot = plot)
+            test <- sim_result$data_df
+
+            expect_equal(test[,4], result$data_df[,4])
+            expect_equal(test[,5], result$data_df[,5])
           })
