c33f76a115b4828363ca270425d837ec73e005ea
Jonathan Lin <jlin900@berkeley.edu>
2023-09-07 13:58:49 -0700

    added spaces and stuff

--- R/resampleClusteredRegions.R
+++ R/resampleClusteredRegions.R
@@ -61,29 +61,29 @@ resampleClusteredRegions <- function(voxel_df, n_pca = 20,
     stop("n_umap and n_pca must be greater than 1")
   }
   else{
-  if (subsamp_prop == 1){
+  if (subsamp_prop == 1 || subsamp_prop == 0){
     stop("subsamp_prop must be less than 1")
   }
   else{
 
   # find the number of columns needed to sample from subsamp_prop
-  num <- floor(ncol(voxel_df[,4:ncol(voxel_df)])*subsamp_prop)
+  num <- floor(ncol(voxel_df[, 4:ncol(voxel_df)])*subsamp_prop)
 
   # create empty data frame which clusters will be stored in later
   cluster_vec <- data.frame(matrix(NA,
                                    nrow = nrow(voxel_df),
                                    ncol = n_resamp))
 
-  voxel_df_data <- voxel_df[,4:ncol(voxel_df)]
+  voxel_df_data <- voxel_df[, 4:ncol(voxel_df)]
 
   # extract xyz coords
-  xyz <- voxel_df[,1:3]
+  xyz <- voxel_df[, 1:3]
 
   # run a for loop to resample and calculate clusters
-  for (i in 1:n_resamp){
+  for (i in 1:n_resamp) {
 
     # sample voxel data from voxel_df
-    subsamp <- voxel_df_data[,sample(ncol(voxel_df_data), size=num)]
+    subsamp <- voxel_df_data[, sample(ncol(voxel_df_data), size = num)]
 
     # bind xyz back to the subsample
     new_voxel_df <- cbind(xyz, subsamp)
@@ -91,7 +91,7 @@ resampleClusteredRegions <- function(voxel_df, n_pca = 20,
     # run dataDrivenClusters() with specified values
     DDC <- suppressMessages(dataDrivenClusters(new_voxel_df, n_pca = n_pca, n_umap = n_umap,
                               n_clust = n_clust, region = region))
-    clusters <- DDC$data_df[,6]
+    clusters <- DDC$data_df[, 6]
 
     # append to cluster_vec
     cluster_vec[,i] <- clusters
@@ -103,10 +103,10 @@ message("Cluster vectors calculated")
     ARI <- matrix(NA, n_resamp, n_resamp)
 
     # reiterate to find pairwise ARIs
-  for (i in 1:n_resamp){
+  for (i in 1:n_resamp) {
     for (j in c(1:n_resamp)[1:n_resamp != i]) {
-      ARI_j <- adjustedRandIndex(cluster_vec[,i], cluster_vec[,j])
-      ARI[i,j] <- ARI_j
+      ARI_j <- adjustedRandIndex(cluster_vec[, i], cluster_vec[, j])
+      ARI[i, j] <- ARI_j
     }
 
   }
@@ -134,7 +134,7 @@ message("Matrix calculated")
       labs(x = "Sample #", y = "Sample #")
 
     # appending to list
-    result <- list(Average = avg, Matrix = plot, Values = ARI)
+    result <- list(average = avg, matrix = plot, values = ARI)
 
   return(result)
 
