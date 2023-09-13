14eb0374fa7a58129778f706372bcb8c6be11e4c
Jonathan Lin <jlin900@berkeley.edu>
2023-09-13 15:16:09 -0700

    new indenting

--- R/featEng-tails.R
+++ R/featEng-tails.R
@@ -40,67 +40,68 @@ computeTailMeans <- function(voxel_df, data_df = NULL, alpha = 0.05) {
 
   if (alpha != 0 & alpha != 1) {
 
-  if (!is.null(data_df)) {
+    if (!is.null(data_df)) {
 
-  # cbind voxel_df and data_df
-  combine_df <- cbind(voxel_df, data_df)
+      # cbind voxel_df and data_df
+      combine_df <- cbind(voxel_df, data_df)
 
-  # extracting the columns in following order: PID, x, y, z, Value, Cluster
-  combind_df <- combine_df[, c(2, 3, 4, 5, 6, 12)]
+      # extracting the columns in following order: PID, x, y, z, Value, Cluster
+      combind_df <- combine_df[, c(2, 3, 4, 5, 6, 12)]
 
-  # adding additional xyz identifier column
-  combine_df <- combine_df %>% mutate(xyz = paste0(combine_df[, 1],", ",combine_df[, 2],", ",combine_df[, 3]))
+      # adding additional xyz identifier column
+      combine_df <- combine_df %>% mutate(xyz = paste0(combine_df[, 1],", ",combine_df[, 2],", ",combine_df[, 3]))
 
-  #removing x, y, z columns since those are redundant after the xyz column.
-  combine_df <- combine_df[, c(1, 5, 6, 7)]
+      #removing x, y, z columns since those are redundant after the xyz column.
+      combine_df <- combine_df[, c(1, 5, 6, 7)]
 
-  num_clusts <- length(unique(combine_df[, 2]))
+      num_clusts <- length(unique(combine_df[, 2]))
 
-  # initializing empty vectors
-  clust_vec <- c()
-  PID_vec <- c()
-  lower_mean_vec <- c()
-  upper_mean_vec <- c()
-  lower_quantile_vec <- c()
-  upper_quantile_vec <- c()
+      # initializing empty vectors
+      clust_vec <- c()
+      PID_vec <- c()
+      lower_mean_vec <- c()
+      upper_mean_vec <- c()
+      lower_quantile_vec <- c()
+      upper_quantile_vec <- c()
 
-  # cycling through each cluster label
-  for (i in 1:num_clusts) {
+      # cycling through each cluster label
+      for (i in 1:num_clusts) {
 
-    # isolating individual clusters
-    filtered_df <- combine_df %>% filter(cluster == i)
+        # isolating individual clusters
+        filtered_df <- combine_df %>% filter(cluster == i)
 
-    # for each cluster, isolate individual PID
-    for(ID in unique(combine_df[, 1])) {
+          # for each cluster, isolate individual PID
+          for(ID in unique(combine_df[, 1])) {
 
-      # filter the dataframe so that we are only considering individual PIDs
-      uniqueid_df <- filtered_df %>% filter(pid == ID)
+          # filter the dataframe so that we are only considering individual PIDs
+          uniqueid_df <- filtered_df %>% filter(pid == ID)
 
-      # find quantile cutoffs
-      quantile_cutoff_lower <- quantile(uniqueid_df[, 3], alpha)
-      quantile_cutoff_upper <- quantile(uniqueid_df[, 3], 1-alpha)
+          # find quantile cutoffs
+          quantile_cutoff_lower <- quantile(uniqueid_df[, 3], alpha)
+          quantile_cutoff_upper <- quantile(uniqueid_df[, 3], 1-alpha)
 
-      # find values that fall beneath and above quantile cutoffs
-      values_lower <- uniqueid_df[, 3][which(uniqueid_df[, 3] < quantile_cutoff_lower)]
-      values_upper <- uniqueid_df[, 3][which(uniqueid_df[, 3] > quantile_cutoff_upper)]
+          # find values that fall beneath and above quantile cutoffs
+          values_lower <- uniqueid_df[, 3][which(uniqueid_df[, 3] < quantile_cutoff_lower)]
+          values_upper <- uniqueid_df[, 3][which(uniqueid_df[, 3] > quantile_cutoff_upper)]
 
-      # finding averages
-      mean_lower <- mean(values_lower)
-      mean_upper <- mean(values_upper)
+          # finding averages
+          mean_lower <- mean(values_lower)
+          mean_upper <- mean(values_upper)
 
-      # appending all values into a vector
-      clust_vec <- c(clust_vec, i)
-      PID_vec <- c(PID_vec, ID)
-      lower_mean_vec <- c(lower_mean_vec, mean_lower)
-      upper_mean_vec <- c(upper_mean_vec, mean_upper)
-      lower_quantile_vec <- c(lower_quantile_vec, quantile_cutoff_lower)
-      upper_quantile_vec <- c(upper_quantile_vec, quantile_cutoff_upper)
+          # appending all values into a vector
+          clust_vec <- c(clust_vec, i)
+          PID_vec <- c(PID_vec, ID)
+          lower_mean_vec <- c(lower_mean_vec, mean_lower)
+          upper_mean_vec <- c(upper_mean_vec, mean_upper)
+          lower_quantile_vec <- c(lower_quantile_vec, quantile_cutoff_lower)
+          upper_quantile_vec <- c(upper_quantile_vec, quantile_cutoff_upper)
 
-    }
-  }
+          }
 
-  # putting all results into a dataframe
-  result <- data.frame(PID = PID_vec,
+        }
+
+      # putting all results into a dataframe
+      result <- data.frame(PID = PID_vec,
                        Cluster = clust_vec,
                        LowerMean = lower_mean_vec,
                        UpperMean = upper_mean_vec,
@@ -109,55 +110,56 @@ computeTailMeans <- function(voxel_df, data_df = NULL, alpha = 0.05) {
 
   }
 
-  else{
+  else {
 
-      new_df <- voxel_df[, c(2:6)]
+    new_df <- voxel_df[, c(2:6)]
 
-      PID_vec <- c()
-      lower_mean_vec <- c()
-      upper_mean_vec <- c()
-      lower_quantile_vec <- c()
-      upper_quantile_vec <- c()
+    PID_vec <- c()
+    lower_mean_vec <- c()
+    upper_mean_vec <- c()
+    lower_quantile_vec <- c()
+    upper_quantile_vec <- c()
 
-      for (ID in unique(new_df[, 1])) {
-        # filter the dataframe so that we are only considering individual PIDs
-        uniqueid_df <- new_df %>% filter(pid == ID)
+    for (ID in unique(new_df[, 1])) {
+      # filter the dataframe so that we are only considering individual PIDs
+      uniqueid_df <- new_df %>% filter(pid == ID)
 
-        # find quantile cutoffs
-        quantile_cutoff_lower <- quantile(uniqueid_df[, 5], alpha)
-        quantile_cutoff_upper <- quantile(uniqueid_df[, 5], 1-alpha)
+      # find quantile cutoffs
+      quantile_cutoff_lower <- quantile(uniqueid_df[, 5], alpha)
+      quantile_cutoff_upper <- quantile(uniqueid_df[, 5], 1-alpha)
 
-        # find values that fall beneath and above quantile cutoffs
-        values_lower <- uniqueid_df[, 5][which(uniqueid_df[, 5] < quantile_cutoff_lower)]
-        values_upper <- uniqueid_df[, 5][which(uniqueid_df[, 5] > quantile_cutoff_upper)]
+      # find values that fall beneath and above quantile cutoffs
+      values_lower <- uniqueid_df[, 5][which(uniqueid_df[, 5] < quantile_cutoff_lower)]
+      values_upper <- uniqueid_df[, 5][which(uniqueid_df[, 5] > quantile_cutoff_upper)]
 
-        # finding averages
-        mean_lower <- mean(values_lower)
-        mean_upper <- mean(values_upper)
+      # finding averages
+      mean_lower <- mean(values_lower)
+      mean_upper <- mean(values_upper)
 
-        # appending all values into a vector
-        PID_vec <- c(PID_vec, ID)
-        lower_mean_vec <- c(lower_mean_vec, mean_lower)
-        upper_mean_vec <- c(upper_mean_vec, mean_upper)
-        lower_quantile_vec <- c(lower_quantile_vec, quantile_cutoff_lower)
-        upper_quantile_vec <- c(upper_quantile_vec, quantile_cutoff_upper)
-      }
+      # appending all values into a vector
+      PID_vec <- c(PID_vec, ID)
+      lower_mean_vec <- c(lower_mean_vec, mean_lower)
+      upper_mean_vec <- c(upper_mean_vec, mean_upper)
+      lower_quantile_vec <- c(lower_quantile_vec, quantile_cutoff_lower)
+      upper_quantile_vec <- c(upper_quantile_vec, quantile_cutoff_upper)
 
-      # putting all results into dataframe
-      result <- data.frame(PID = PID_vec,
-                           LowerMean = lower_mean_vec,
-                           UpperMean = upper_mean_vec,
-                           LowerQuantile = lower_quantile_vec,
-                           UpperQuantile = upper_quantile_vec)
+    }
 
-  }
+    # putting all results into dataframe
+    result <- data.frame(PID = PID_vec,
+                          LowerMean = lower_mean_vec,
+                          UpperMean = upper_mean_vec,
+                          LowerQuantile = lower_quantile_vec,
+                          UpperQuantile = upper_quantile_vec)
+    }
 
   }
 
   else {
-    stop("alpha cannot be 0 or 1")
-  }
 
+    stop("alpha cannot be 0 or 1")
+
+  }
 
   return(result)
 
