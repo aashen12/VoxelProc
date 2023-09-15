45d9ca751149321361a99207b11d6eaca82e763b
Andy Shen <aashen@berkeley.edu>
2023-09-15 11:49:57 -0700

    add skeleton code for entropy

---
+++ R/featEng-entropy.R
@@ -0,0 +1,8 @@
+# this function computes entropy as a feature
+# follow the same procedure as the tail means
+
+
+computeRegionEntropy <- function() {
+  # your code here
+  # need to use the functions entropy::entropy and entropy::discretize
+}
