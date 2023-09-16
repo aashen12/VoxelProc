785373540b6dbe2e9e02b1c8d887e8bcdb02e981
Jonathan Lin <jlin900@berkeley.edu>
2023-09-15 19:37:39 -0700

    skeleton for entropy function

--- R/featEng-entropy.R
+++ R/featEng-entropy.R
@@ -1,10 +1,30 @@
-# this function computes entropy as a feature
-# follow the same procedure as the tail means
+#' @import entropy
+#'
+#' @export
 
 
-computeRegionEntropy <- function() {
-  # your code here
-  # need to use the functions entropy::entropy and entropy::discretize
-  # set bins = floor(sqrt(num_voxels)) in that region
-  # see example code on gDrive and test with the same test data
+computeRegionEntropy <- function(voxel_df, data_df, sep_clusters = TRUE) {
+
+  combine_df <- cbind(voxel_df, data_df[, 4:6]) %>% tibble()
+
+  if (sep_clusters){
+  result <- combine_df %>%
+    mutate_at(vars("cluster"), factor) %>%
+    group_by(pid, cluster) %>%
+    mutate(nvox = n()) %>%
+    ungroup() %>%
+    mutate(avg_vox = mean(nvox)) %>%
+    group_by(pid, cluster) %>%
+    summarise(entropy = entropy::entropy(entropy::discretize(value, numBins = floor(sqrt(avg_vox[1]))))
+              )
+  }
+
+  else{
+  result <- combine_df %>%
+    mutate_at(vars("cluster"), factor) %>%
+    group_by(pid) %>%
+    mutate(nvox = n()) %>%
+    summarise(entropy = entropy::entropy(entropy::discretize(value, numBins = floor(sqrt(nvox[1]))))
+              )
+  }
 }
