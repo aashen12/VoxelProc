bfc0ea60a0183c8602a741beba0e8d78ec4ea6d6
Jonathan Lin <jlin900@berkeley.edu>
2023-09-21 15:36:44 -0700

    fixed for 4-col id_mapping

--- R/mergeData.R
+++ R/mergeData.R
@@ -6,10 +6,10 @@ mergeData <- function(feat_eng_df, id_mapping = NULL, test_clinical_data) {
 
   # note: still need to generalize this to more colnames.
   if (!is.null(id_mapping)) {
-  colnames(id_mapping) <- c("PatID", "pid")
+  colnames(id_mapping) <- c("SubjID", "PatID", "pid", "DataDir")
   df_merged <- left_join(feat_eng_df, id_mapping, by = "pid") %>%
     dplyr::relocate(PatID, .after = "pid") %>%
-    dplyr::select(-pid)
+    dplyr::select(-pid, -SubID, -DataDir)
   result <- left_join(df_merged, test_clinical_data, by = "PatID") %>%
     na.omit()
   }
