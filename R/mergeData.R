09d94837865283f9e7052da7993349d326a0c971
Jonathan Lin <jlin900@berkeley.edu>
2023-10-22 16:31:33 -0700

    removed dependency on specific colnames

--- R/mergeData.R
+++ R/mergeData.R
@@ -16,12 +16,14 @@
 #' @param feat_eng_df A \code{tibble} that takes the same form as an output of
 #' \code{computeRegionEntropy()}.
 #' @param test_clinical_data A \code{dataframe} that includes the clinical data
-#' of the patients.
+#' of the patients. It is expected for the first column to be a patient
+#' identification column.
 #' @param match A \code{character} indicating which column name corresponds to
-#' the identificaton seen in \code{feat_eng_df} and \code{id_mapping}
-#' @param id_mapping A \code{dataframe} that has at least the columns 'PatID' and
-#' match, the latter of which is an argument in the function. If 'match' is not
-#' specified, then the function will assume that 'ScrID' is the second column name.
+#' the identificaton seen in \code{feat_eng_df} and \code{id_mapping}.
+#' @param id_mapping A \code{dataframe} that has at least the columns 'match' and
+#' the patient ID seen in \code{clinical_data}, the former of which is an argument
+#' in the function. If 'match' is not specified, then the function will assume that '
+#' ScrID' is the first column name.
 #'
 #' @returns A \code{tibble} that outputs the original clinical dataframe with an
 #' added entropy column.
@@ -38,7 +40,7 @@ mergeData <- function(feat_eng_df, clinical_data, match = "ScrID", id_mapping = 
   id_index <- grep(match, colnames(id_mapping))
   id_mapping[, id_index] <- as.factor(id_mapping[, id_index])
   feat_eng_df$pid <- as.factor(feat_eng_df$pid)
-  clinical_data$PatID <- as.factor(clinical_data$PatID)
+  clinical_data[[1]] <- as.factor(clinical_data[[1]])
 
   # isolating column of interest and changing its name to pid
   id_mapping <- id_mapping %>% rename("pid" = match)
