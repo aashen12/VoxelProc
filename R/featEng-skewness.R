2a9d76362532d38c5dbc3890f6601dd21b05c6b6
Jonathan Lin <jlin900@berkeley.edu>
2023-10-25 12:53:58 -0700

    added function for computing skewness of regions

---
+++ R/featEng-skewness.R
@@ -0,0 +1,21 @@
+#' @import moments
+
+computeRegionSkewness <- function(voxel_df, data_df = NULL) {
+
+  if (!is.null(data_df)) {
+    combine_df <- cbind(voxel_df, data_df[, 4:6]) %>% tibble()
+    result <- combine_df %>%
+      mutate_at(vars("cluster"), factor) %>%
+      group_by(pid, cluster) %>%
+      summarize(skewness = skewness(value))
+  }
+
+
+  else {
+    result <- voxel_df %>%
+      group_by(pid) %>%
+      summarize(skewness = skewness(value))
+  }
+
+  return(result)
+}
