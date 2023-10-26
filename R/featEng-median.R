3a249c282803bc33006e38d5791c23ec042790e8
Jonathan Lin <jlin900@berkeley.edu>
2023-10-26 14:43:31 -0700

    removing colname dependency

--- R/featEng-median.R
+++ R/featEng-median.R
@@ -1,7 +1,7 @@
 computeRegionMedian <- function(voxel_df, data_df = NULL) {
 
   if (!is.null(data_df)) {
-    combine_df <- cbind(voxel_df, data_df[, 4:6]) %>% tibble()
+    combine_df <- cbind(voxel_df, data_df[c("U1", "U2", "cluster")]) %>% tibble()
     result <- combine_df %>%
       mutate_at(vars("cluster"), factor) %>%
       group_by(pid, cluster) %>%
