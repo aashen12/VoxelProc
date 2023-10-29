c3f5dd287206f13e03516dab78ed42d24b57fde0
Jonathan Lin <jlin900@berkeley.edu>
2023-10-29 11:21:54 -0700

    feature engineering for proportions

---
+++ R/featEng-proportion.R
@@ -0,0 +1,19 @@
+computeRegionProp <- function(voxel_df, base = 1, epsilon = 0.05, data_df = NULL) {
+
+  if (!is.null(data_df)) {
+    combine_df <- cbind(voxel_df, data_df[c("U1", "U2", "cluster")]) %>% tibble()
+    result <- combine_df %>%
+      mutate_at(vars("cluster"), factor) %>%
+      group_by(pid, cluster) %>%
+      summarize(proportion = sum(value >= base - epsilon & value <= base + epsilon)/length(value))
+  }
+
+
+  else {
+    result <- voxel_df %>%
+      group_by(pid) %>%
+      summarize(proportion = sum(value >= base - epsilon & value <= base + epsilon)/length(value))
+  }
+
+  return(result)
+}
