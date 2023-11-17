b9bf40a1d81b02200cf9f90a73890da2fca65b2c
Jonathan Lin <jlin900@berkeley.edu>
2023-11-17 10:39:13 -0800

    added lasso and ridge

--- R/kfold-cv.R
+++ R/kfold-cv.R
@@ -8,17 +8,48 @@ crossValidation <- function(clinical_data,
                             method = "coxph") {
 
   df <- clinical_data[-1]
-  proportion <- nrow(clinical_data)/k
+  proportion <- 1-1/k
 
-  for (i in 1:k) {
-    inTrain <- createDataPartition(y = clinical_data$time,
-                                   p = proportion,
-                                   list = FALSE)
-    train <- df[inTrain, ]
-    test <- df[-inTrain, ]
-    coxph_model <- coxph(Surv(time, status) ~., data = train)
-    test_pred <- coxph_model %>% predict(test)
-    cindex <- concordance.index(test_pred, test$time, test$status)
+  if (method == "coxph") {
+    cvector <- c()
+    for (i in 1:k) {
+      inTrain <- createDataPartition(y = df$time,
+                                    p = proportion,
+                                    list = FALSE)
+      train <- df[inTrain, ]
+      test <- df[-inTrain, ]
+      coxph_model <- coxph(Surv(time, status) ~., data = train)
+      test_pred <- coxph_model %>% predict(test)
+      cindex <- concordance.index(test_pred, test$time, test$status)
+      cvector <- c(cvector, cindex)
+    }
+    result <- cvector()
   }
-
+  else if (method == "LASSO") {
+    scale_data <- scale(df, center = TRUE)
+    covariates.w.time <- scale_data[, -which(names(scale_data) %in% c("status"))]
+    status <- scale_data["status"]
+    cvfit <- cv.glmnet(covariates.w.time,
+                       status,
+                       nfolds = k,
+                       alpha = 1,
+                       family = "cox",
+                       type.measure = "C")
+    cverror <- cvfit$cvm
+    result <- cverror
+  }
+  else if (method == "Ridge") {
+    scale_data <- scale(df, center = TRUE)
+    covariates.w.time <- scale_data[, -which(names(scale_data) %in% c("status"))]
+    status <- scale_data["status"]
+    cvfit <- cv.glmnet(covariates.w.time,
+                       status,
+                       nfolds = k,
+                       alpha = 0,
+                       family = "cox",
+                       type.measure = "C")
+    cverror <- cvfit$cvm
+    result <- cverror
+  }
+  return(result)
 }
