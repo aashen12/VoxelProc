754af24f3fb1ec97ae4584da16077a7ac72c8813
Jonathan Lin <jlin900@berkeley.edu>
2023-11-25 16:30:48 -0800

    work in progress, not complete yet

--- R/kfold-cv.R
+++ R/kfold-cv.R
@@ -12,37 +12,60 @@ crossValidation <- function(clinical_data,
   df <- clinical_data[, !names(clinical_data) %in% id]
   proportion <- 1-1/k
   if (method == "coxph") {
-    cvector <- rep(NA, k)
-    for (i in 1:k) {
-      inTrain <- createDataPartition(y = df$time,
-                                    p = proportion,
-                                    list = FALSE)
-      train <- df[inTrain, ]
-      test <- df[-inTrain, ]
-      coxph_model <- coxph(Surv(time, status) ~., data = train)
-      test_pred <- coxph_model %>% predict(test)
-      cindex <- concordance.index(test_pred, test$time, test$status)$c.index
-      cvector[i] <- cindex
+    scores <- rep(NA, n)
+    for (j in 1:n) {
+      cvector <- rep(NA, k)
+      for (i in 1:k) {
+        inTrain <- createDataPartition(y = df$time, #figure out how to create actual partitions
+                                      p = proportion,
+                                      list = FALSE)
+        train <- df[inTrain, ]
+        test <- df[-inTrain, ]
+        coxph_model <- coxph(Surv(time, status) ~., data = train)
+        test_pred <- coxph_model %>% predict(test)
+        cindex <- concordance.index(test_pred, test$time, test$status)$c.index
+        cvector[i] <- cindex
+      }
+      score <- mean(cvector)
+      scores[j] <- score
     }
-    result <- list(score = mean(cvector),
-                   concordances = cvector,
-                   sd = sd(cvector))
+    result <- list(scores = scores,
+                   mean = mean(scores),
+                   sd = sd(scores))
   }
   else if (method == "lasso") {
     df <- df %>% select(where(is.numeric))
-    scale_data <- as.data.frame(scale(df, center = TRUE))
-    covariates <- scale_data[, -which(names(scale_data) %in% c("status", "time"))] %>% as.matrix()
-    status <- df["status"][[1]]
-    time <- df["time"][[1]]
-    cvfit <- cv.glmnet(covariates,
+    status_time_col <- df[c("time", "status")]
+    rest_of_df <- df[, -which(names(df) %in% c("status", "time"))]
+    scale_data <- as.data.frame(scale(rest_of_df, center = TRUE))
+    scale_data_full <- cbind(scale_data, status_time_col)
+    for (j in 1:n) {
+      for (i in 1:k) {
+        inTrain <- createDataPartition(y = df$time, #figure out how to create actual partitions
+                                       p = proportion,
+                                       list = FALSE)
+        train <- scale_data_full[inTrain, ]
+        test <- scale_data_full[-inTrain, ]
+        test_covariates <- test[, -which(colnames(test) %in% c("status", "time"))] %>% as.matrix()
+        time <- train["time"][[1]]
+        status <- train["status"][[1]]
+        covariates <- train[, -which(colnames(train) %in% c("status", "time"))] %>% as.matrix()
+        cvfit <- cv.glmnet(covariates,
                        Surv(time, status),
                        nfolds = k,
                        alpha = 1,
                        family = "cox",
                        type.measure = "C")
-    result <- list(score = mean(cvfit$cvm),
-                   concordances = cvfit$cvm,
-                   sd = sd(cvfit$cvm))
+        lambda <- cvfit$lambda.min
+        trained_model <- glmnet(covariates,
+                                Surv(time, status),
+                                family = "cox",
+                                alpha = 1,
+                                lambda = lambda)
+        test_pred <- trained_model %>% predict(test_covariates)
+
+      }
+    }
   }
   else if (method == "Ridge") {
     df <- df %>% select(where(is.numeric))
